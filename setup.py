6da1c89dbb5b1d714a11ab78d3ca569aa25a83a7
catebmartin <catebmartin@gmail.com>
2018-07-12 16:07:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup
 
+setup(name='pyQUE',
+      version='0.1',
+      description='Connector to QUE',
+      url='https://github.com/catebmartin/pyQUE',
+      author='Cat Martin',
+      author_email='catebmartin@gmail.com',
+      license='MIT',
+      packages=['pyQUE'],
+      zip_safe=False)
